e6c1464f821ded3e1a8d8f136fa5358243b41536
Stefanie Bernardin <bernardin.stefanie@gmail.com>
2024-12-09 13:08:07 +0100

    format changes

--- rssreader.py
+++ rssreader.py
@@ -1,23 +1,27 @@
 import feedparser
 
-print('Type in an URL of a RSS Feed:')
+print('Type in an URL of a RSS Feed:') # get url from user
 inputUrl = input().strip()
 
-
-myFeed = feedparser.parse(inputUrl)
-print('\n\nRSS Feed "' + myFeed.feed.title + '"')
+myFeed = feedparser.parse(inputUrl) # parse url and print title of RSS feed
+print(f'\nRSS Feed: {myFeed.feed.title}')
 
 inputYN = 'y'
 myRange = 0
-while inputYN == 'y':
+
+while inputYN == 'y': # while reader wants to read, print the next five items of the feed
     myRange +=5
-    for i in range(myRange-5, myRange):
+    for i in range(myRange-5, myRange): # get title, date, description and link of every item of feed and print it nicely
         myTitle = myFeed.entries[i].title
-        myDate = myFeed.entries[i].published
+        myDate = myFeed.entries[i].published_parsed
+        myDate = f"{myDate[2]:02}.{myDate[1]:02}.{myDate[0]} {myDate[3]:02}:{myDate[4]:02}" # format date
         myDescr = myFeed.entries[i].description
+        myDescr = myDescr[myDescr.find(">")+1:].strip() # some items include pictures in <> tags, delete them
         myLink = myFeed.entries[i].link
-        print('\n', myTitle, " (", myDate, ")")
+
+        print(f"\n{myDate} {myTitle}")
         print(myDescr)
-        print(myLink)
-    print("Do you like to see more? Type y for yes, n for no:")
+        print("Read more: ", myLink)
+
+    print("\nDo you like to see more? Type y for yes, n for no:") # does the user want to read more?
     inputYN = input().strip()
